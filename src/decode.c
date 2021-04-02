9f1df28b27bf24cfbaa684d8de3457008f9334c5
neelfirst <neelfirst@gmail.com>
2021-04-01 23:28:51 -0700

    working c prototype, no packet checking

--- src/decode.c
+++ src/decode.c
@@ -1,26 +1,67 @@
-#include <fstream>
-#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-START0 = b'21'
-START1 = b'22'
-HEADER = [START0, START1]
-
-def get_length(packet):
-def ingest(file):
-def get_packets(hexlist):
-def valid(packet):
-def set_format(packet):
+const unsigned char START0 = 0x21;
+const unsigned char START1 = 0x22;
 
 int main (int argc, char **argv)
 {
-  parser = argparse.ArgumentParser("Process sample serial data, stored in binary hex format.")
-  parser.add_argument("--input", type=str)
-  parser.add_argument("--output", type=str)
-  args = parser.parse_args()
+  if (argc != 5 || strcmp(argv[1],"--input") != 0 || strcmp(argv[3],"--output") != 0) {
+    printf("Usage: decode --input <input_file> --output <output_file>\n");
+    exit(1);
+  }
 
-  # convert input file to array of hex bytes
-  hex_in = ingest(args.input)
+  FILE *infile = fopen(argv[2],"rb");
+  FILE *outfile = fopen(argv[4],"w");
+  unsigned char byte[1];
 
+  // The only reason to write in C is for performance, so let's
+  // dispense with the niceties and write a single loop.
+
+  bool seekHeader = false;
+  bool gotHeader = false;
+
+  // Rreading one byte at a time incurs I/O overhead, but it seems the
+  // problem is geared to simulate real-time byte retrieval and analysis,
+  // so this loop would most likely run bytewise in a serial callback.
+
+  while (fread(byte,sizeof(char),1,infile) == 1)
+  {
+    if (gotHeader) {
+      gotHeader = false;
+      unsigned int length = (unsigned int)(byte[0]);
+      unsigned char packet[length];
+      if (fread(packet, length, 1, infile) == length) {
+        printf("eof or packet read error");
+        break;
+      }
+      else {
+        fprintf(outfile, "{ %03u } ", length);
+        for (int i=0; i<length; i++) fprintf(outfile, "%02X ", packet[i]);
+        fprintf(outfile, "\n");
+      }
+    }
+
+    if (seekHeader && byte[0] == START1) {
+      gotHeader = true;
+      printf("got header\n");
+    }
+    else gotHeader = false;
+
+    if (byte[0] == START0) {
+      seekHeader = true;
+      printf("got start0, now seeking header\n");
+    }
+    else seekHeader = false;
+  }
+
+
+  // convert input file to array of hex bytes
+  // unsigned char *hex_in = ingest(infile)
+
+/*
   # get all packets as array of arrays
   packets = get_packets(hex_in)
 
@@ -29,5 +70,7 @@ int main (int argc, char **argv)
     for packet in packets:
       if valid(packet):
         f.write(set_format(packet))
+*/
   return 0;
 }
+
